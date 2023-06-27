9b89c3c63bed9d5169329605e0489bb40e590752
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-27 11:55:34 +0100

    Compiled all separate file into appropraite ones

--- handler_help4.c
+++ handler_help4.c
@@ -20,3 +20,43 @@ int put_reverse(char *s)
 	return (retval);
 
 }
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: string to encode
+ *
+ * Return: Encoded and decoded string
+ */
+int rot13(char *str)
+{
+	int i, j, k = 0;
+	char rot13_dict[52][2] = { {'A', 'N'}, {'B', 'O'}, {'C', 'P'}, {'D', 'Q'},
+				   {'E', 'R'}, {'F', 'S'}, {'G', 'T'}, {'H', 'U'},
+				   {'I', 'V'}, {'J', 'W'}, {'K', 'X'}, {'L', 'Y'},
+				   {'M', 'Z'}, {'N', 'A'}, {'O', 'B'}, {'P', 'C'},
+				   {'Q', 'D'}, {'R', 'E'}, {'S', 'F'}, {'T', 'G'},
+				   {'U', 'H'}, {'V', 'I'}, {'W', 'J'}, {'X', 'K'},
+				   {'Y', 'L'}, {'Z', 'M'}, {'a', 'n'}, {'b', 'o'},
+				   {'c', 'p'}, {'d', 'q'}, {'e', 'r'}, {'f', 's'},
+				   {'g', 't'}, {'h', 'u'}, {'i', 'v'}, {'j', 'w'},
+				   {'k', 'x'}, {'l', 'y'}, {'m', 'z'}, {'n', 'a'},
+				   {'o', 'b'}, {'p', 'c'}, {'q', 'd'}, {'r', 'e'},
+				   {'s', 'f'}, {'t', 'g'}, {'u', 'h'}, {'v', 'i'},
+				   {'w', 'j'}, {'x', 'k'}, {'y', 'l'}, {'z', 'm'}, };
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == rot13_dict[j][0])
+			{
+				str[i] = rot13_dict[j][1];
+				break;
+			}
+		}
+	}
+	while (str[k])
+		_putchar(str[k++]);
+
+	return (k);
+}
+
--- handlers3.c
+++ handlers3.c
@@ -33,3 +33,21 @@ int print_rev(va_list ap)
 		return (_puts("(null)"));
 	return (put_reverse(str));
 }
+
+/**
+ * print_rot - prints the rot13'ed string
+ * @ap: The argument pointer to the string
+ *
+ * Return: The numbers of bytes printed
+ */
+int print_rot(va_list ap)
+{
+	char *str;
+
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		return (_puts("(null)"));
+
+
+	return (rot13(str));
+}
